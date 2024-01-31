205ea82074a7e7d52812e5fe4e11eafc632e4bc6
31daylee <gussla2206@gmail.com>
2024-01-31 14:04:37 +0900

    data query updated

--- mybank/0129 data.sql
+++ mybank/0129 data.sql
@@ -16,3 +16,16 @@ update account_tb set number= '0111', password = '2222', balance = 9000
 insert into history_tb
 	(amount, w_account_id,d_account_id,w_balance,d_balance,created_at)
 values(1000,1,3, 300,1000,now());
+
+# 출금 금액 insert 쿼리문 만들기 
+use mybank;
+desc history_tb;
+
+# 출금 내역(1번 계좌에서 100원)
+insert into history_tb(amount, w_account_id, d_account_id,
+					w_balance, d_balance, created_at)
+values(100,1,null,1200,null, now());
+
+# 출금 처리 update
+update account_tb set number = '1111', password = '1234', 
+	balance = 1200, user_id='1' where id = '1';
