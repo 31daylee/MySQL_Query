0d9c5b7a3ab506cb6f4ed29ab123745dd9741e6d
31daylee <gussla2206@gmail.com>
2024-02-02 17:32:05 +0900

    240202 1일 1쿼리

--- 02 Feb/240202 1일 1쿼리.sql
+++ 02 Feb/240202 1일 1쿼리.sql
@@ -1,1 +1,15 @@
-#문제1) 각 부서별 80년대 연도별 engineer 직원수를 모두 구하라(engineer 들어가는 모든 직급 포함)
+#문제1) 각 부서별 engineer 직원수를 모두 구하라(engineer 들어가는 모든 직급 포함)
+select de.dept_no, count(t.emp_no) as 'Engineer Employees'
+from titles as t
+join dept_emp as de on t.emp_no = de.emp_no
+where title like '%engineer%'
+group by de.dept_no
+order by de.dept_no;
+
+-- 위와 성능 동일 
+select de.dept_no, count(t.emp_no) as 'Engineer Employees'
+from dept_emp as de
+join titles as t on t.emp_no = de.emp_no
+where title like '%engineer%'
+group by de.dept_no
+order by de.dept_no;
